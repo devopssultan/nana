17a630dc909c4eb2465cfd7672ce09cfa3feabd6
sultan <devopssultan@gmail.com>
2021-12-12 11:21:47 +0530

    mvn project

---
+++ src/main/java/devopsbuild/App.java
@@ -0,0 +1,13 @@
+package devopsbuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
